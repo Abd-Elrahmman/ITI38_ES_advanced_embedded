4b4f843303d6db81cc0b3fb5050d160ec89cca81
Abd-Elrahmman <abdelrahmanmousa16@gmail.com>
2018-03-20 19:18:02 +0200

    unrelated files has been removed (ie. settings, debug etc)

--- Debug/UART_Rx_TEST.d
+++
@@ -1,8 +0,0 @@
-UART_Rx_TEST.o UART_Rx_TEST.o: ../UART_Rx_TEST.c ../STD_TYPES.h \
-  ../DIO_int.h ../UART.h
-
-../STD_TYPES.h:
-
-../DIO_int.h:
-
-../UART.h:
--- Debug/UART_cfg.d
+++
@@ -1,5 +0,0 @@
-UART_cfg.o UART_cfg.o: ../UART_cfg.c ../STD_TYPES.h ../UART_cfg.h
-
-../STD_TYPES.h:
-
-../UART_cfg.h:
